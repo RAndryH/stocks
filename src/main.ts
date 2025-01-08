ec15ddd024e72d3040ea36a845048c26413e30c2
andry <randryh@gmail.com>
2025-01-08 18:55:22 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',  // Allow only your frontend origin
+    origin: 'https://stocksreactredux.vercel.app/',  // Allow only your frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
