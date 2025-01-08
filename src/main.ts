283c4454adbf26eab68c1c5134ca24a516ff21b9
andry <randryh@gmail.com>
2025-01-08 19:02:21 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://stocksreactredux.vercel.app/',  // Allow only your frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
